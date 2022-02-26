cb34bdf6edc88c83f8c3cd2fbafb15faa83e581a
Sumit <sumitsapkota0@gmail.com>
2022-02-26 21:04:09 +0545

    initial commit

---
+++ components/atoms/LetterCard/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+    width:40px;
+    height:40px;
+    background-color:#552696;
+`;
+
+const LetterCard = () => {
+  return (
+      <Wrapper>
+          <div>LetterCard</div>
+      </Wrapper>
+  )
+}
+
+export {LetterCard}
